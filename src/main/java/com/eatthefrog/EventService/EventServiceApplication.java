d88d8a43cdbcd4cbc4231a00d8f06fa2d64a60ac
John Burns <johnb1003@gmail.com>
2022-09-26 11:27:06 -0600

    Spring Initializr

---
+++ src/main/java/com/eatthefrog/EventService/EventServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eatthefrog.EventService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventServiceApplication.class, args);
+	}
+
+}
